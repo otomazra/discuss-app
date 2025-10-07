2f0db0002c608ab72321d11d3625b83cd6a4237d
otomazra <oto.mazra@gmail.com>
2025-10-08 00:34:16 +0400

    Add code for saving the created topics in database through prisma, revalidating the homepage after it and redirecting to the created topic page

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,6 +1,11 @@
 "use server";
 import { z } from "zod";
 import { auth } from "@/auth";
+import type {Topic} from "@prisma/client";
+import {redirect} from "next/navigation";
+import {db} from "@/db";
+import paths from "@/paths";
+import {revalidatePath} from "next/cache";
 
 const createTopicSchema = z.object({
   name: z
@@ -25,12 +30,17 @@ export async function createTopic(
   formState: CreateTopicFormState,
   formData: FormData
 ): Promise<CreateTopicFormState> {
-  const session = await auth();
-  const result = createTopicSchema.safeParse({
-    name: formData.get("name"),
-    description: formData.get("description"),
-  });
-
+    const result = createTopicSchema.safeParse({
+        name: formData.get("name"),
+        description: formData.get("description"),
+    });
+    
+    if (!result.success) {
+      console.log(result.error.flatten().fieldErrors);
+      return { errors: result.error.flatten().fieldErrors };
+    }
+    
+    const session = await auth();
   if(!session || !session.user){
     return {
         errors: {
@@ -39,12 +49,31 @@ export async function createTopic(
     }
   }
 
-  if (!result.success) {
-    console.log(result.error.flatten().fieldErrors);
-    return { errors: result.error.flatten().fieldErrors };
+  let topic: Topic;
+  try{
+    topic = await db.topic.create({
+        data: {
+            slug: result.data.name,
+            description: result.data.description,
+        },
+    });
+  }catch(err: unknown){
+    if(err instanceof Error){
+        return {
+            errors: {
+                _form: [err.message],
+            },
+        }
+    }else {
+        return {
+            errors: {
+                _form: ["Something went wrong"],
+            },
+        }
+    }
   }
-  return {
-    errors: {},
-  };
+
   // TODO: revalidate the homepage
+  revalidatePath('/');
+  redirect(paths.topicShow(topic.slug));
 }
