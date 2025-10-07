6fbc82b713096fb4a02027a3d493fcc0eef39385
otomazra <oto.mazra@gmail.com>
2025-10-07 21:06:12 +0400

    Add zod validation, add handleSubmit function to onSubmit form prop to avoid auto reset of the forms

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,4 +1,38 @@
-'use server';
-export async function createTopic(){
-    // TODO: revalidate the homepage
-};
+"use server";
+import { z } from "zod";
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, {
+      message:
+        "Must be lowercase, without numbers and special signs except dash",
+    }),
+  description: z.string().min(10),
+});
+
+interface CreateTopicFormState {
+  errors: {
+    name?: Array<string>;
+    description?: Array<string>;
+  };
+}
+
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
+  const result = createTopicSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description"),
+  });
+  if (!result.success) {
+    console.log(result.error.flatten().fieldErrors);
+    return {errors: result.error.flatten().fieldErrors};
+  }
+  return {
+    errors: {}
+  }
+  // TODO: revalidate the homepage
+}
--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -1,3 +1,6 @@
+'use client';
+import type { JSX } from "react";
+import {useActionState, startTransition} from "react";
 import {
   Popover,
   PopoverTrigger,
@@ -8,7 +11,18 @@ import {
 } from "@nextui-org/react";
 import * as actions from "@/actions";
 
-export default function TopicCreateForm() {
+export default function TopicCreateForm(): JSX.Element {
+  const [formState, action] = useActionState(actions.createTopic, {
+    errors: {},
+  });
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    startTransition(()=>{
+      action(formData);
+    });
+  };
+
   return (
     <Popover placement="left">
       <PopoverTrigger>
@@ -17,7 +31,7 @@ export default function TopicCreateForm() {
         </Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form action={actions.createTopic}>
+        <form onSubmit={handleSubmit} noValidate>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-lg">Create a Topic</h3>
             <Input label="Name" labelPlacement="outside" placeholder="Name" />
