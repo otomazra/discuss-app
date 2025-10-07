44654e3a72f120178f67c82139a504fc50c25ebc
otomazra <oto.mazra@gmail.com>
2025-10-08 00:35:54 +0400

    nothing changed

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,11 +1,11 @@
 "use server";
 import { z } from "zod";
 import { auth } from "@/auth";
-import type {Topic} from "@prisma/client";
-import {redirect} from "next/navigation";
-import {db} from "@/db";
+import type { Topic } from "@prisma/client";
+import { redirect } from "next/navigation";
+import { db } from "@/db";
 import paths from "@/paths";
-import {revalidatePath} from "next/cache";
+import { revalidatePath } from "next/cache";
 
 const createTopicSchema = z.object({
   name: z
@@ -30,50 +30,50 @@ export async function createTopic(
   formState: CreateTopicFormState,
   formData: FormData
 ): Promise<CreateTopicFormState> {
-    const result = createTopicSchema.safeParse({
-        name: formData.get("name"),
-        description: formData.get("description"),
-    });
-    
-    if (!result.success) {
-      console.log(result.error.flatten().fieldErrors);
-      return { errors: result.error.flatten().fieldErrors };
-    }
-    
-    const session = await auth();
-  if(!session || !session.user){
+  const result = createTopicSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description"),
+  });
+
+  if (!result.success) {
+    console.log(result.error.flatten().fieldErrors);
+    return { errors: result.error.flatten().fieldErrors };
+  }
+
+  const session = await auth();
+  if (!session || !session.user) {
     return {
-        errors: {
-            _form: ['You must be signed in to do this']
-        }
-    }
+      errors: {
+        _form: ["You must be signed in to do this"],
+      },
+    };
   }
 
   let topic: Topic;
-  try{
+  try {
     topic = await db.topic.create({
-        data: {
-            slug: result.data.name,
-            description: result.data.description,
-        },
+      data: {
+        slug: result.data.name,
+        description: result.data.description,
+      },
     });
-  }catch(err: unknown){
-    if(err instanceof Error){
-        return {
-            errors: {
-                _form: [err.message],
-            },
-        }
-    }else {
-        return {
-            errors: {
-                _form: ["Something went wrong"],
-            },
-        }
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        errors: {
+          _form: [err.message],
+        },
+      };
+    } else {
+      return {
+        errors: {
+          _form: ["Something went wrong"],
+        },
+      };
     }
   }
 
   // TODO: revalidate the homepage
-  revalidatePath('/');
+  revalidatePath("/");
   redirect(paths.topicShow(topic.slug));
 }
