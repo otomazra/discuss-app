35bf74ed565d41fb882ea86bc0d6af8cf62d39e5
otomazra <oto.mazra@gmail.com>
2025-10-12 23:57:00 +0400

    Add topic-list component that renders the list of topics on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 import TopicCreateForm from "@/components/topics/topic-create-form";
+import TopicList from "@/components/topics/topic-list";
+import { Divider } from "@nextui-org/react";
 
 export default async function Home() {
   return (
     <div className="grid grid-cols-4 gap-4 p-4">
       <div className="col-span-3">
         <h1 className="text-lx m-2" >Topic List</h1>
-
       </div>
-      <div>
+      <div className="border shadow py-3 px-2">
         <TopicCreateForm />
+        <Divider className="my-2"></Divider>
+        <h3 className="text-lg" >Topics</h3>
+        <TopicList />
       </div>
     </div>
   )
